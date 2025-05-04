719a9661dd078f42d0b4c926a00f84842adbba2a
ahmed-soltan <soa548277@gmail.com>
2025-05-04 14:30:53 +0300

    handling deployment on netlify

--- src/features/projects/api/use-get-repo.ts
+++ src/features/projects/api/use-get-repo.ts
@@ -1,23 +1,23 @@
-import { client } from "@/lib/rpc";
-import { useQuery } from "@tanstack/react-query";
+// import { client } from "@/lib/rpc";
+// import { useQuery } from "@tanstack/react-query";
 
-export const useGetRepo = () => {
-  const query = useQuery({
-    queryKey: ["repos"],
-    queryFn: async () => {
-      const response = await  await client.api.projects["repos"]["$get"]();
+// export const useGetRepo = () => {
+//   const query = useQuery({
+//     queryKey: ["repos"],
+//     queryFn: async () => {
+//       const response = await  await client.api.projects["repos"]["$get"]();
 
-      if (!response.ok) {
-        return null;
-      }
+//       if (!response.ok) {
+//         return null;
+//       }
 
-      console.log(response);
+//       console.log(response);
 
-      const data = await response.json();
+//       const data = await response.json();
 
-      return data;
-    },
-  });
+//       return data;
+//     },
+//   });
 
-  return query;
-};
+//   return query;
+// };
--- src/features/projects/components/project-github-repo.tsx
+++ src/features/projects/components/project-github-repo.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { useGetRepo } from '../api/use-get-repo'
 
 const ProjectGithubRepo = () => {
-    const {data} = useGetRepo();
-
-    console.log(data)
+  
   return (
     <div>ProjectGithubRepo</div>
   )
