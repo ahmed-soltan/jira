782113b47a8e9c95e15316d9d257b788b15bfb65
ahmed-soltan <soa548277@gmail.com>
2025-05-04 14:40:32 +0300

    handling deployment on netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    swcMinify: true,
+  };
+  
+  export default nextConfig;
+  
